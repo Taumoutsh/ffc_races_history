4b1ec00c1201fc746366bd134311ad1594ea64be
Taumoutsh <55583455+Taumoutsh@users.noreply.github.com>
2025-09-07 20:34:23 +0200

    [CLEANUP] Remove unused files

--- test-constants.js
+++
@@ -1,5 +0,0 @@
-import { COLORS, TYPOGRAPHY } from './src/constants/index.js';
-
-console.log('COLORS:', Object.keys(COLORS));
-console.log('TYPOGRAPHY:', Object.keys(TYPOGRAPHY));
-console.log('PRIMARY_GRADIENT:', COLORS.PRIMARY_GRADIENT);
